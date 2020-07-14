19ac6519a83f9830f2810005c74fb615bda446e3
EstevesAndre <estevesandre1@hotmail.com>
2020-07-14 02:26:34 +0100

    Add theme 1 palette

---
+++ eigenstate/lib/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class Themes {
+  static Color p1Grey = Color(0xff3f5363);
+  static Color p1Yellow = Color(0xffe68a0d);
+  static Color p1Orange = Color(0xffe38250);
+  static Color p1Purple = Color(0xff2d2b53);
+  static Color p1Blue = Color(0xff498c9c);
+}
