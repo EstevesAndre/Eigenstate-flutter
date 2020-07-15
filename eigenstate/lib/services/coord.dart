de73410a2919f828351b16d1d98273629c7caa3e
EstevesAndre <estevesandre1@hotmail.com>
2020-07-16 00:16:16 +0100

    Implement coordinate class

---
+++ eigenstate/lib/services/coord.dart
@@ -0,0 +1,29 @@
+
+class Coordinate {
+  int i;
+  int j;
+
+  Coordinate.origin() {
+    this.i = 0;
+    this.j = 0;
+  }
+
+  Coordinate(this.i, this.j);
+
+  void setCoordinates(int i, int j) {
+    this.i = i;
+    this.j = j;
+  }
+
+  int getI() {
+    return i;
+  }
+
+  int getJ() {
+    return j;
+  }
+
+  void clean() {
+    i = j = 0;
+  }
+}
