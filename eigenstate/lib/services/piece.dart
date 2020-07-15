67dbfc9313a70cfd28c30784274714165fd2d086
EstevesAndre <estevesandre1@hotmail.com>
2020-07-16 00:17:06 +0100

    Change initial piece pins active vs disable

--- eigenstate/lib/services/piece.dart
+++ eigenstate/lib/services/piece.dart
@@ -1,39 +1,59 @@
 import 'package:rxdart/rxdart.dart';
+import 'package:eigenstate/services/board.dart';
 
-enum Owner { P1, P2, Empty }
 enum Pin { Center, Active, Disable }
 
 class PieceService {
-
   BehaviorSubject<int> _id$;
   BehaviorSubject<int> get id$ => _id$;
 
-  BehaviorSubject<Owner> _own$;
-  BehaviorSubject<Owner> get own$ => _own$;
+  BehaviorSubject<Player> _own$;
+  BehaviorSubject<Player> get own$ => _own$;
 
   BehaviorSubject<List<List<Pin>>> _piece$;
   BehaviorSubject<List<List<Pin>>> get piece$ => _piece$;
 
-  PieceService(int _id, Owner _own) {
+  PieceService(int _id, Player _own) {
     _initStreams(_id, _own);
   }
 
   PieceService.empty() {
-    _own$ = BehaviorSubject<Owner>.seeded(Owner.Empty);
+    _own$ = BehaviorSubject<Player>.seeded(null);
   }
 
-  void _initStreams(int _id, Owner _own) {
+  bool checkDestinationReachable(int i, int j) {
+    // TODO missing verifications
+
+    return true;
+  }
+
+  List<List<Pin>> getPins() {
+    return _piece$.value;
+  }
+
+  void _initStreams(int _id, Player _own) {
     _piece$ = BehaviorSubject<List<List<Pin>>>.seeded([
       [Pin.Disable, Pin.Disable, Pin.Disable, Pin.Disable, Pin.Disable],
-      [Pin.Disable, Pin.Disable, Pin.Disable, Pin.Disable, Pin.Disable],
+      [
+        Pin.Disable,
+        Pin.Disable,
+        _own == Player.P1 ? Pin.Active : Pin.Disable,
+        Pin.Disable,
+        Pin.Disable
+      ],
       [Pin.Disable, Pin.Disable, Pin.Center, Pin.Disable, Pin.Disable],
-      [Pin.Disable, Pin.Disable, Pin.Disable, Pin.Disable, Pin.Disable],
+      [
+        Pin.Disable,
+        Pin.Disable,
+        _own == Player.P2 ? Pin.Active : Pin.Disable,
+        Pin.Disable,
+        Pin.Disable
+      ],
       [Pin.Disable, Pin.Disable, Pin.Disable, Pin.Disable, Pin.Disable]
     ]);
 
     _id$ = BehaviorSubject<int>.seeded(_id);
 
-    _own$ = BehaviorSubject<Owner>.seeded(_own);
-
+    _own$ = BehaviorSubject<Player>.seeded(_own);
   }
 }
