58bf6f3ea04513e16c0446bae144952317844b7c
EstevesAndre <estevesandre1@hotmail.com>
2020-07-16 00:16:29 +0100

    Add more theme colors

--- eigenstate/lib/theme/theme.dart
+++ eigenstate/lib/theme/theme.dart
@@ -6,4 +6,8 @@ class Themes {
   static Color p1Orange = Color(0xffe38250);
   static Color p1Purple = Color(0xff2d2b53);
   static Color p1Blue = Color(0xff498c9c);
+  static Color p1PinSelected = Color(0xff14de21);
+  static Color p1PinEmpty = Color(0xffebebeb);
+  static Color p2PinSelected = Color(0xff0fdba8);
+  static Color p2PinEmpty = Color(0xffc8d1be);
 }
