e96cee1c95d275310968488526d56996350372e8
EstevesAndre <estevesandre1@hotmail.com>
2020-07-21 03:52:59 +0100

    Add animation system to piece service

--- eigenstate/lib/theme/theme.dart
+++ eigenstate/lib/theme/theme.dart
@@ -4,6 +4,7 @@ class Themes {
   static Color p1Grey = Color(0xff3f5363);
   static Color p1Yellow = Color(0xffe68a0d);
   static Color p1Orange = Color(0xffe38250);
+  static Color p1Orange2 = Color(0xffe38f50);
   static Color p1Purple = Color(0xff2d2b53);
   static Color p1Blue = Color(0xff498c9c);
   static Color p1PinSelected = Color(0xff14de21);
